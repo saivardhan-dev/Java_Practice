3088b94d8e3edf6cd1ede6962dfc9842646209c1
saivardhan <saivardhanreddynarra@Sais-MacBook-Air-4.local>
2025-11-25 10:46:07 -0600

    How to Handle Exception

---
+++ ExceptionHandling/src/ArrayOutOfBound.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class ArrayOutOfBound {
+    public static void main(String[] args){
+        try {
+            int[] arr = {1, 2, 3, 4, 5};
+            System.out.println(arr[5]);
+        }catch(ArrayIndexOutOfBoundsException e){
+            System.out.println("Error=" + e);
+        }
+    }
+}
