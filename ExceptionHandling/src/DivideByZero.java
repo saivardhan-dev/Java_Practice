05f08440103cbb32e069e2dac5a264fb70cad375
saivardhan <saivardhanreddynarra@Sais-MacBook-Air-4.local>
2025-11-25 10:23:53 -0600

    How to Handle Exception

---
+++ ExceptionHandling/src/DivideByZero.java
@@ -0,0 +1,12 @@
+public class DivideByZero {
+    public static void main(String[] args){
+        try{
+            int a=10;
+            int b=0;
+            int c=a/b;
+            System.out.println("c=" + c);
+        }catch(ArithmeticException e){
+            System.out.println("Error: " + e);
+        }
+    }
+}
