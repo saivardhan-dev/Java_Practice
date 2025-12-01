3ec3e8e96163e1480aa44fbfab80d2db1b60c4c8
saivardhan <saivardhanreddynarra@Sais-MacBook-Air-4.local>
2025-12-01 16:01:42 -0600

    String Handling

--- StringHandling/src/Main.java
+++ StringHandling/src/Main.java
@@ -5,14 +5,12 @@ Take a Paragraph and
 3. No. of words -
 4. No. of letters -
 5. No. of letters by alphabets
-6. no. of special characters, symbols, or punctuation
+6. No. of special characters, symbols, or punctuation
 
 In Java, java.lang.Throwable is the superclass of all errors and exceptions. Only objects that are instances of Throwable (or one of its subclasses) can be thrown by the Java Virtual Machine or by the throw statement in your code. Similarly, only Throwable or its subclasses can be used as the argument type in a catch clause to handle exceptions.
 */
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args){
@@ -23,7 +21,7 @@ public class Main {
         int count = 0;
         int noOfSpaces=0;
         int noOfLetters=0;
-        String[] words = input.split(" ");
+        String[] words = input.split("[^a-zA-Z]+");
 
         for(int i = 0; i < n; i++) {
             count++;
@@ -42,18 +40,14 @@ public class Main {
 
 
 //        char[] ch = input.toCharArray();
-//        boolean[] b = new boolean[n];
 //
 //        System.out.println("Character Occurrences:");
 //        for (int i = 0; i < ch.length; i++) {
-//            if (b[i]) continue;
 //            int charCount=1;
 //            for(int j = i + 1; j < ch.length; j++) {
 //                if (ch[i] == ch[j]) {
 //                    charCount++;
-//                    b[j] = true;
 //                }
-//
 //            }
 //            System.out.println(ch[i] + " : " + charCount);
 //        }
@@ -66,9 +60,6 @@ public class Main {
                 map.put(c, 1);
             }
         }
-        System.out.println("Character Occurrences:");
-        for(Map.Entry<Character, Integer> entry : map.entrySet()){
-            System.out.println(entry.getKey() + " : " + entry.getValue());
-        }
+        System.out.println("Character Occurrences: " + map);
     }
 }
