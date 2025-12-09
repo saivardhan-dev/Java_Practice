b44546ca4503ac4e2a8d1673a4ae544ced665975
saivardhan <saivardhanreddynarra@Sais-MacBook-Air-4.local>
2025-12-09 10:57:18 -0600

    Practicing Threads

--- PlayWithJava/src/Main.java
+++ PlayWithJava/src/Main.java
@@ -1,74 +1,85 @@
-/*
-Take a Paragraph and
-1. Find the number of char in the string -
-2. No. of spaces -
-3. No. of words -
-4. No. of letters -
-5. No. of letters by alphabets
-6. no. of special characters, symbols, or punctuation
+import java.util.LinkedList;
+import java.util.List;
 
-In Java, java.lang.Throwable is the superclass of all errors and exceptions. Only objects that are instances of Throwable (or one of its subclasses) can be thrown by the Java Virtual Machine or by the throw statement in your code. Similarly, only Throwable or its subclasses can be used as the argument type in a catch clause to handle exceptions.
-*/
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
-
-public class Main {
-    public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
-
-        int n=input.length();
-        int count = 0;
-        int noOfSpaces=0;
-        int noOfLetters=0;
-        String[] words = input.split(" ");
-
-        for(int i = 0; i < n; i++) {
-            count++;
-            if(input.charAt(i) == ' '){
-                noOfSpaces++;
-            }
-            else{
-                noOfLetters++;
-            }
+class Runway{
+    private final int capacity;
+    private final List<String> aircraftStand=new LinkedList<>();
+    public Runway(int capacity){
+        this.capacity=capacity;
+    }
+    public synchronized void landing(String plane) throws InterruptedException{
+        while (aircraftStand.size()==capacity) {
+            wait();
         }
-
-        System.out.println("Number of char in the string: " + count);
-        System.out.println("Number of Spaces in the string: " + noOfSpaces);
-        System.out.println("Number of letters in the string: " + noOfLetters);
-        System.out.println("Number of Words in the string: " + words.length);
-
-
-        char[] ch = input.toCharArray();
-        boolean[] b = new boolean[n];
-
-        System.out.println("Character Occurrences:");
-        for (int i = 0; i < ch.length; i++) {
-          if (b[i]) continue;
-          int charCount=1;
-            for(int j = i + 1; j < ch.length; j++) {
-                if (ch[i] == ch[j]) {
-                    charCount++;
-                    b[j] = true;
-                }
-
-            }
-            System.out.println(ch[i] + " : " + charCount);
+        aircraftStand.add(plane);
+        System.out.println("Landing: "+plane);
+        notifyAll();
+    }
+    public synchronized String onBoarding() throws InterruptedException{
+        while(aircraftStand.isEmpty()){
+            wait();
         }
+        String plane=aircraftStand.remove(0);
+        System.out.println("Onboarding: "+plane);
+        notifyAll();
+        return plane;
+    }
+}
 
-//        Map<Character, Integer> map = new HashMap<>();
-//        for(char c : input.toCharArray()) {
-//            if(map.containsKey(c)){
-//                map.put(c, map.get(c)+1);
-//            }else{
-//                map.put(c, 1);
-//            }
-//        }
-//        System.out.println("Character Occurrences:");
-//        for(Map.Entry<Character, Integer> entry : map.entrySet()){
-//            System.out.println(entry.getKey() + " : " + entry.getValue());
-//        }
+class Incoming implements Runnable{
+    private final Runway runway;
+    private final int totalPlanes;
+
+    public Incoming(Runway runway, int totalPlanes){
+        this.runway=runway;
+        this.totalPlanes=totalPlanes;
+    }
+
+    @Override
+    public void run() {
+        try {
+            for (int i = 1; i <= totalPlanes; i++) {
+                String name = "plane= " + i;
+                runway.landing(name);
+                Thread.sleep(100);
+            }
+        }catch(InterruptedException ignored){}
+    }
+}
+class Outgoing implements Runnable{
+    private final Runway runway;
+    private final int toProcess;
+
+    public Outgoing(Runway runway, int toProcess){
+        this.runway=runway;
+        this.toProcess=toProcess;
+    }
+
+    @Override
+    public void run(){
+        try{
+            for(int i = 1; i <= toProcess; i++){
+                String name = runway.onBoarding();
+                System.out.println("TakeOff: " + name);
+                Thread.sleep(150);
+            }
+        }catch(InterruptedException ignored){}
+    }
+}
+
+class Main{
+    public static void main(String[] args) throws InterruptedException{
+        int capacity=3;
+        int totalPlanes=10;
+
+        Runway aircraftStand = new Runway(capacity);
+
+        Thread outgoing = new Thread(new Incoming(aircraftStand, totalPlanes),"Outgoing");
+        Thread incoming = new Thread(new Outgoing(aircraftStand, totalPlanes),"Incoming");
+
+        outgoing.start();
+        incoming.start();
+        outgoing.join();
+        incoming.join();
     }
 }
--- PracticingThreads/src/Main.java
+++ PracticingThreads/src/Main.java
@@ -1,25 +1,85 @@
-class t1 extends Thread{
-    public void run(){
-        for(int i = 0; i < 10; i++) {
-            System.out.println("Hello t1!");
+import java.util.LinkedList;
+import java.util.List;
+
+class Runway{
+    private final int capacity;
+    private final List<String> aircraftStand=new LinkedList<>();
+    public Runway(int capacity){
+        this.capacity=capacity;
+    }
+    public synchronized void landing(String plane) throws InterruptedException{
+        while (aircraftStand.size()==capacity) {
+            wait();
         }
+        aircraftStand.add(plane);
+        System.out.println("Landing: "+plane);
+        notifyAll();
+    }
+    public synchronized String onBoarding() throws InterruptedException{
+        while(aircraftStand.isEmpty()){
+            wait();
+        }
+        String plane=aircraftStand.remove(0);
+        System.out.println("Onboarding: "+plane);
+        notifyAll();
+        return plane;
     }
 }
 
-class t2 implements Runnable{
+class Incoming implements Runnable{
+    private final Runway runway;
+    private final int totalPlanes;
+
+    public Incoming(Runway runway, int totalPlanes){
+        this.runway=runway;
+        this.totalPlanes=totalPlanes;
+    }
+
+    @Override
     public void run() {
-        for(int i = 0; i < 10; i++) {
-            System.out.println("Hola t2!");
-        }
+        try {
+            for (int i = 1; i <= totalPlanes; i++) {
+                String name = "plane= " + i;
+                runway.landing(name);
+                Thread.sleep(100);
+            }
+        }catch(InterruptedException ignored){}
+    }
+}
+class Outgoing implements Runnable{
+    private final Runway runway;
+    private final int toProcess;
+
+    public Outgoing(Runway runway, int toProcess){
+        this.runway=runway;
+        this.toProcess=toProcess;
+    }
+
+    @Override
+    public void run(){
+        try{
+            for(int i = 1; i <= toProcess; i++){
+                String name = runway.onBoarding();
+                System.out.println("TakeOff: " + name);
+                Thread.sleep(150);
+            }
+        }catch(InterruptedException ignored){}
     }
 }
 
 class Main{
     public static void main(String[] args) throws InterruptedException{
-        Thread t2 = new Thread(new t2());
-        t2.start();
-        t2.join();
-        t1 thread = new t1();
-        thread.start();
+        int capacity=3;
+        int totalPlanes=10;
+
+        Runway aircraftStand = new Runway(capacity);
+
+        Thread outgoing = new Thread(new Incoming(aircraftStand, totalPlanes),"Outgoing");
+        Thread incoming = new Thread(new Outgoing(aircraftStand, totalPlanes),"Incoming");
+
+        outgoing.start();
+        incoming.start();
+        outgoing.join();
+        incoming.join();
     }
 }
